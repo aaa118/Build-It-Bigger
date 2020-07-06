611b57d8d1e99e06de001f722c4f64648ded5c83
Adi <aaavs118@gmail.com>
2020-07-06 00:39:38 -0400

    Added paid flavor

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -32,7 +32,16 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-        AdView mAdView = root.findViewById(R.id.adView);
+        if (!BuildConfig.IS_PAID) {
+            AdView mAdView = root.findViewById(R.id.adView);
+            // Create an ad request. Check logcat output for the hashed device ID to
+            // get test ads on a physical device. e.g.
+            // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+            AdRequest adRequest = new AdRequest.Builder()
+                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                    .build();
+            mAdView.loadAd(adRequest);
+        }
         Button button = root.findViewById(R.id.bt_tellJoke);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,13 +55,7 @@ public class MainActivityFragment extends Fragment {
                 startActivity(intent);
             }
         });
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
+
         return root;
     }
 }
